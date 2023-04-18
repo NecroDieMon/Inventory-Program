f40127e2f80226d8bd2869e1710e58df13009c33
04.12.22 <04.12.22@HOME-PC>
2023-04-18 23:25:54 +0300

    Добавьте файлы проекта.

---
+++ PracticWork/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace PracticWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.condition_txt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inventnum_txt = new System.Windows.Forms.TextBox();
+            this.equip_txt = new System.Windows.Forms.TextBox();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.depart_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Insert_btn = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.GetWarning = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.condition_txt);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.inventnum_txt);
+            this.panel1.Controls.Add(this.equip_txt);
+            this.panel1.Controls.Add(this.name_txt);
+            this.panel1.Controls.Add(this.depart_txt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(25, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(366, 335);
+            this.panel1.TabIndex = 5;
+            // 
+            // condition_txt
+            // 
+            this.condition_txt.Location = new System.Drawing.Point(124, 268);
+            this.condition_txt.Name = "condition_txt";
+            this.condition_txt.Size = new System.Drawing.Size(212, 20);
+            this.condition_txt.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(18, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Состояние";
+            // 
+            // inventnum_txt
+            // 
+            this.inventnum_txt.Location = new System.Drawing.Point(205, 62);
+            this.inventnum_txt.Name = "inventnum_txt";
+            this.inventnum_txt.Size = new System.Drawing.Size(143, 20);
+            this.inventnum_txt.TabIndex = 5;
+            // 
+            // equip_txt
+            // 
+            this.equip_txt.Location = new System.Drawing.Point(157, 209);
+            this.equip_txt.Name = "equip_txt";
+            this.equip_txt.Size = new System.Drawing.Size(191, 20);
+            this.equip_txt.TabIndex = 6;
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(73, 115);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(263, 20);
+            this.name_txt.TabIndex = 5;
+            // 
+            // depart_txt
+            // 
+            this.depart_txt.Location = new System.Drawing.Point(90, 160);
+            this.depart_txt.Name = "depart_txt";
+            this.depart_txt.Size = new System.Drawing.Size(194, 20);
+            this.depart_txt.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ФИО";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(18, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Инвентарный номер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(19, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Отдел";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(18, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Оборудование";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel2.Controls.Add(this.Insert_btn);
+            this.panel2.Controls.Add(this.Delete);
+            this.panel2.Controls.Add(this.Update);
+            this.panel2.Location = new System.Drawing.Point(25, 382);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(366, 76);
+            this.panel2.TabIndex = 10;
+            // 
+            // Insert_btn
+            // 
+            this.Insert_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Insert_btn.Location = new System.Drawing.Point(3, 12);
+            this.Insert_btn.Name = "Insert_btn";
+            this.Insert_btn.Size = new System.Drawing.Size(115, 52);
+            this.Insert_btn.TabIndex = 6;
+            this.Insert_btn.Text = "Добавить";
+            this.Insert_btn.UseVisualStyleBackColor = true;
+            this.Insert_btn.Click += new System.EventHandler(this.Insert_btn_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Delete.Location = new System.Drawing.Point(245, 12);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(115, 52);
+            this.Delete.TabIndex = 8;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Update
+            // 
+            this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Update.Location = new System.Drawing.Point(124, 12);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(115, 52);
+            this.Update.TabIndex = 7;
+            this.Update.Text = "Изменить";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // GetWarning
+            // 
+            this.GetWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GetWarning.Location = new System.Drawing.Point(897, 388);
+            this.GetWarning.Name = "GetWarning";
+            this.GetWarning.Size = new System.Drawing.Size(102, 70);
+            this.GetWarning.TabIndex = 12;
+            this.GetWarning.Text = "Обратите внимание!";
+            this.GetWarning.UseVisualStyleBackColor = true;
+            this.GetWarning.Click += new System.EventHandler(this.GetWarning_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(470, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(544, 335);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(466, 373);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(187, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "93XXXXXX - Мебель ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(466, 409);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(229, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "94XXXXXX - Электроника";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(1026, 480);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.GetWarning);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Инвентаризация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox inventnum_txt;
+        private System.Windows.Forms.TextBox equip_txt;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox depart_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Insert_btn;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.TextBox condition_txt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button GetWarning;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ PracticWork/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Runtime.CompilerServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+using dllMesage;
+
+namespace PracticWork
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SQLiteConnection baglan = new SQLiteConnection("Data source=PrakRab.db");
+
+        public void listele()
+        {
+            baglan.Open();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("Select * From DBPrakRab", baglan);
+            DataSet dset = new DataSet();
+            adapter.Fill(dset, "info");
+            dataGridView1.DataSource = dset.Tables[0];
+            baglan.Close();
+
+        }
+
+        class DBconText
+        {
+            public DBcon DBcon { get; set; }
+            public void showtext(string dbText)
+            {
+                DBcon = DBcon.getInstance(dbText);
+            }
+        }
+        class DBcon
+        {
+            private static DBcon instance;
+
+            public string ShowTextDB { get; private set; }
+
+            protected DBcon(string showtextdb)
+            {
+                this.ShowTextDB = showtextdb;
+            }
+
+            public static DBcon getInstance(string showtextdb)
+            {
+                if (instance == null)
+                    instance = new DBcon(showtextdb);
+                return instance;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DBconText dbcontex = new DBconText();
+            dbcontex.showtext("База данных PrakRab успешно подключена");
+            MessageBox.Show(dbcontex.DBcon.ShowTextDB);
+            listele();
+        }
+
+        private void Insert_btn_Click(object sender, EventArgs e) //Кнопка добавить
+        {
+            baglan.Open();
+            string inventnum = Convert.ToString(inventnum_txt.Text);
+            string fio = Convert.ToString(name_txt.Text);
+            string depart = Convert.ToString(depart_txt.Text);
+            string equip = Convert.ToString(equip_txt.Text);
+            string condition = Convert.ToString(condition_txt.Text);
+            string sql = "insert into DBPrakRab(InventoryNumber, FIO, Department, Equipment, Condition) Values ('" + inventnum + "', '" + fio + "' , '" + depart + "' , '" + equip + "', '" + condition + "')";
+            SQLiteCommand komutislet = new SQLiteCommand(sql, baglan);
+            komutislet.ExecuteNonQuery();
+            baglan.Close();
+            listele();
+        }
+        private void Update_Click(object sender, EventArgs e) //Кнопка изменить
+        {
+            baglan.Open();
+            string inventnum = Convert.ToString(inventnum_txt.Text);
+            string fio = Convert.ToString(name_txt.Text);
+            string depart = Convert.ToString(depart_txt.Text);
+            string equip = Convert.ToString(equip_txt.Text);
+            string condition = Convert.ToString(condition_txt.Text);
+            string sqlInvNumb = "UPDATE DBPrakRab SET InventoryNumber='" + inventnum + "'";
+            string sqlFIO = "UPDATE DBPrakRab SET FIO='" + fio + "'";
+            string sqlDepartment = "UPDATE DBPrakRab SET Department='" + depart + "'";
+            string sqlEquipment = "UPDATE DBPrakRab SET Equipment='" + equip + "'";
+            string sqlCondition = "UPDATE DBPrakRab SET Condition='" + condition + "'";
+            SQLiteCommand komutisletInvNumb = new SQLiteCommand(sqlInvNumb, baglan);
+            komutisletInvNumb.ExecuteNonQuery();
+            SQLiteCommand komutisletFIO = new SQLiteCommand(sqlFIO, baglan);
+            komutisletFIO.ExecuteNonQuery();
+            SQLiteCommand komutisletDepartment = new SQLiteCommand(sqlDepartment, baglan);
+            komutisletDepartment.ExecuteNonQuery();
+            SQLiteCommand komutisletEquipment = new SQLiteCommand(sqlEquipment, baglan);
+            komutisletEquipment.ExecuteNonQuery();
+            SQLiteCommand komutisletCondition = new SQLiteCommand(sqlCondition, baglan);
+            komutisletCondition.ExecuteNonQuery();
+            baglan.Close();
+            listele();
+        }
+        private void Delete_Click(object sender, EventArgs e) //Кнопка удалить
+        {
+            baglan.Open();
+            string fio = Convert.ToString(name_txt.Text);
+            string sql = "DELETE  FROM DBPrakRab WHERE FIO='"+fio+"'";
+            SQLiteCommand komutislet = new SQLiteCommand(sql, baglan);
+            komutislet.ExecuteNonQuery();
+            baglan.Close();
+            listele();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            inventnum_txt.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            name_txt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            depart_txt.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            equip_txt.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            condition_txt.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void GetWarning_Click(object sender, EventArgs e)
+        {
+            MessageTitle messageTitle = new MessageTitle();
+            messageTitle.getMessage();
+        }
+    }
+}
